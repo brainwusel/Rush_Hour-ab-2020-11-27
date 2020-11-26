89730076064d20e7e8b3c5e0acbbe664d0757dfb
brainwusel <aig09@uni-koeln.de>
2020-11-26 22:43:31 +0100

    Initial Commit

---
+++ Rush_Hour/Model.swift
@@ -0,0 +1,194 @@
+//
+//  Model.swift
+//  Rush_Hour
+//
+//  Created by UT on 20.11.20.
+//
+
+import Foundation
+import Cocoa
+
+// 6 x 6
+// 4 Dreier + 4 Zweier
+// von LINKS nach RECHTS waagerecht w =  1 2 3 4 5 6
+// von UNTEN nach OBEN nach senkrecht s = 1 2 3 4 5 6
+// Ausgang rechts neben w = 6 / s = 3
+// CAVE im zweidimensionalen Array SPIEL.GRID, welches die einzelnen Zellen das 6 x 6 Spielfeldes repräsentiert, sind die Indizes der einzelnen Felder um den Wert 1 KLEINER (0 ... 5, da Array) als die Positionsbezeichnung in der KLasse CAR (w bzw. s 1...6)
+
+// Länge des Autos
+enum Länge: Int {
+    case zwei = 2
+    case drei = 3
+}
+
+// Orientierung des Autos auf dem Spielfeld
+enum Orientierung: String {
+    case waagerecht = "w"
+    case senkrecht = "s"
+}
+
+// Bewegungsrichtung: Optionen für Autos bzw. Input über Maus / Taste
+enum Richtung {
+    case rechts
+    case links
+    case rauf
+    case runter
+    case unbeweglich
+}
+
+// einzelnes Auto - identifiziert durch Länge und Farbe in der computet prop. ID als String. Aus der Aufgabenstellung ergeben sich die Eigenschaften POSITIONLINKSOBEN und ORIENTIERUNG. Die Eigenschaft BEWEGUNGSOPTIONEN zeigt als Array von Richtungen an, wohin ein Auto bewegt werden könnte; sie wird zunächst mit einem Element .UNBEWEGLICH initialisiert und später durch die Methode SPIEL.BEWEGUNGSOPTIONENUPDATE aus den Positionsdaten aller Autos berechnet. Die Eigenschaft ZELLENBELEGT zeigt an, welche Zellen gerade von einem Auto im Grid belegt werden und wird berechnet aus der POSITIONLINKSOBEN, der LÄNGE und der ORIENTIERUNG des Autos; in der get-Funktion sind KEINE Überprüfungen der waagerechten oder senkrechten Limits enthalten - diese Aufgabe sollte vollständig von der Methode SPIEL.BEWEGUNSOPTIONENUPDATE übernommen werden
+class Car {
+    var länge: Länge
+    var farbe: NSColor
+    var orientierung: Orientierung
+    var positionLinksUnten: (w: Int, s: Int)
+    var bewegungsOptionen: [Richtung]
+    
+    init (länge: Länge, farbe: NSColor, positionLinksUnten: (w: Int, s: Int), richtung: Orientierung) {
+        self.länge = länge
+        self.farbe = farbe
+        self.orientierung = richtung
+        self.positionLinksUnten = positionLinksUnten
+        self.bewegungsOptionen = [.unbeweglich]
+    }
+    
+    var zellenBelegt: [(w: Int, s: Int)] {
+        get {
+            var zB = [(w: Int, s: Int)]()
+            zB.append(positionLinksUnten)
+            
+            if länge == .zwei && orientierung == .waagerecht {
+                zB.append((w: positionLinksUnten.w + 1, s: positionLinksUnten.s))
+            }
+            if länge == .zwei && orientierung == .senkrecht {
+                zB.append((w: positionLinksUnten.w, s: positionLinksUnten.s + 1))
+            }
+            if länge == .drei && orientierung == .waagerecht {
+                zB.append((w: positionLinksUnten.w + 1, s: positionLinksUnten.s))
+                zB.append((w: positionLinksUnten.w + 2, s: positionLinksUnten.s))
+            }
+            if länge == .drei && orientierung == .senkrecht {
+                zB.append((w: positionLinksUnten.w, s: positionLinksUnten.s + 1))
+                zB.append((w: positionLinksUnten.w, s: positionLinksUnten.s + 2))
+            }
+            return zB
+        }
+    }
+    
+    var id: String {
+        get {
+            return ("\(self.länge)/\(self.farbe)")
+        }
+    }
+}
+
+//  Aufgabe 1 aus ScreenShot - eine Logik für die Auswahl verschiedender Aufgaben fehlt noch
+//  standardisierte Reihenfolge der Autos im Auto-Array: "Zweier" rot gelb grün blau "Dreier" rot gelb grün blau, "Zweier" rot ist das Exit-Auto
+func initAutos () -> [Car] {
+    var autos = [Car]()
+    
+    autos.append(Car(länge: .zwei, farbe: .red, positionLinksUnten: (w: 2, s: 3), richtung: .waagerecht))    // Index 0 = Exit-Auto
+    autos.append(Car(länge: .zwei, farbe: .yellow, positionLinksUnten: (w: 1, s: 1), richtung: .waagerecht)) // Index 1
+    autos.append(Car(länge: .zwei, farbe: .green, positionLinksUnten: (w: 5, s: 5), richtung: .waagerecht))  // Index 2
+    autos.append(Car(länge: .zwei, farbe: .blue, positionLinksUnten: (w: 1, s: 5), richtung: .senkrecht))    // Index 3
+    
+    autos.append(Car(länge: .drei, farbe: .red, positionLinksUnten: (w: 1, s: 2), richtung: .senkrecht))     // Index 4
+    autos.append(Car(länge: .drei, farbe: .yellow, positionLinksUnten: (w: 6, s: 1), richtung: .senkrecht))  // Index 5
+    autos.append(Car(länge: .drei, farbe: .green, positionLinksUnten: (w: 3, s: 6), richtung: .waagerecht))  // Index 6
+    autos.append(Car(länge: .drei, farbe: .blue, positionLinksUnten: (w: 4, s: 2), richtung: .senkrecht))    // Index 7
+    
+    return autos
+}
+
+
+class Spiel {
+    
+    //  alle Autos aus dem Rush Hour ScreenShot = Aufgabe1
+    var cars: [Car] = initAutos()
+    
+    //  Belegung der einzelnen Zellen durch Auto-ID (String aus LÄNGE und FARBE), falls leer: " ". Die Funktion überprüft NICHT, ob die Autos korrekt platziert wurden; das sollte vollständig durch die Methode BEWEGUNGSOPTIONENUPDATE sichergestellt werden. Es setzt auch voraus, dass die Aufgabenstellung korrekt formuliert ist.
+    var grid: [[String]] {
+        get {
+            var g = [[String]](repeating: [String](repeating: " ", count: 6), count: 6) // 6 x 6 - alle Felder mit " " belegen
+            for auto in cars {
+                for pos in auto.zellenBelegt {
+                    g[pos.w - 1][pos.s - 1] = auto.id
+                }
+            }
+            return g
+        }
+    }
+    
+    //    verschiebe ein bestimmtes Auto um eine Zelle, Identifizierung des Autos über den String ID
+    func move (autoID: String, wohin: Richtung) {      // AUTOID: und WOHIN: werden von Maus- bzw. Tastatursignalen bestimmt
+        bewegungsOptionenUpdate(cars: &cars)
+        for auto in cars {
+            if auto.id == autoID {
+                if auto.bewegungsOptionen.contains(wohin) {
+                    switch wohin {
+                    case .rechts:
+                        auto.positionLinksUnten.w += 1
+                    case .links:
+                        auto.positionLinksUnten.w -= 1
+                    case .rauf:
+                        auto.positionLinksUnten.s += 1
+                    case .runter:
+                        auto.positionLinksUnten.s -= 1
+                    default:
+                        return
+                    }
+                }
+            }
+        }
+        //aktualisiert die Eigenschaft ZELLENBELEGT für jedes Autos, aus der sich automatisch das GRID berechnet
+        bewegungsOptionenUpdate(cars: &cars)
+    }
+    
+    //    wohin kann welches Auto bewegt werden?
+    func bewegungsOptionenUpdate (cars: inout [Car]) {
+        for auto in cars {
+            
+            let links = auto.zellenBelegt.first?.w
+            let rechts = auto.zellenBelegt.last?.w
+            let unten = auto.zellenBelegt.first?.s
+            let oben = auto.zellenBelegt.last?.s
+            
+            if auto.orientierung == .waagerecht {
+                
+                if links! > 1 && grid[links! - 2][unten! - 1] == " " {
+                    auto.bewegungsOptionen.append(.links)
+                }
+                if rechts! < 6 && grid[rechts!][oben! - 1] == " " {
+                    auto.bewegungsOptionen.append(.rechts)
+                }
+                if rechts! == 6 && oben! == 3 {                    // Exit!
+                    auto.bewegungsOptionen.append(.rechts)
+                }
+            }
+            
+            if auto.orientierung == .senkrecht {
+                if unten! > 1 && grid[links! - 1][unten! - 2] == " " {
+                    auto.bewegungsOptionen.append(.runter)
+                }
+                if oben! < 6 && grid[rechts! - 1][oben!] == " " {
+                    auto.bewegungsOptionen.append(.rauf)
+                }
+            }
+        }
+    }
+    
+    //    welches Auto belegt eine bestimmte Zelle?
+    func autoAufZelle (waagerecht: Int, senkrecht: Int) -> Car? {       // waagerecht, senkrecht: 1 ... 6
+        let identifier = grid[waagerecht - 1][senkrecht - 1]
+        var auto: Car? = nil
+        for c in cars {
+            if c.id == identifier {
+                auto = c
+            }
+        }
+        return auto                                                     // gibt NIL aus, wenn Zelle nicht von Auto belegt
+    }
+}
+
+
+
---
+++ Rush_Hour/myView.swift
@@ -0,0 +1,126 @@
+//
+//  myView.swift
+//  Rush_Hour
+//
+//  Created by UT on 24.11.20.
+//
+
+import Cocoa
+
+class myView: NSView {
+    
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+        
+        // Hintergrund
+        let re_HinterGrund = NSRect(x: 0, y: 0, width: dirtyRect.width, height: dirtyRect.height)
+        let re_HinterGrundPath = NSBezierPath()
+        re_HinterGrundPath.appendRect(re_HinterGrund)
+        NSColor.white.setFill()
+        re_HinterGrundPath.fill()
+        
+        // Basisquadrat
+        _ = erzeugeBasisQuadrat(füllFarbe: .white, randFarbe: .black, zeichnen: true, dirtyRect)
+        
+        // kleine Quadrate -> Grid
+        for w in 1 ... 6 {
+            for s in 1 ... 6 {
+                _ = erzeugeKleinesQuadrat(waagerecht: w, senkrecht: s, farbe: .lightGray, zeichnen: true, dirtyRect)
+            }
+        }
+        
+        // Autos
+        let spiel = Spiel()
+        let cars = spiel.cars
+        for auto in cars {
+            let re_linksUnten = erzeugeKleinesQuadrat(
+                waagerecht: (auto.zellenBelegt.first?.w)!,
+                senkrecht: (auto.zellenBelegt.first?.s)!,
+                farbe: .white,
+                zeichnen: false,
+                dirtyRect)
+            let re_rechtsOben = erzeugeKleinesQuadrat(
+                waagerecht: (auto.zellenBelegt.last?.w)!,
+                senkrecht: (auto.zellenBelegt.last?.s)!,
+                farbe: .white,
+                zeichnen: false,
+                dirtyRect)
+            let autoPath = NSBezierPath()
+            autoPath.appendRect(NSRect(
+                                    x: re_linksUnten.minX + 10,
+                                    y: re_linksUnten.minY + 10,
+                                    width: re_rechtsOben.maxX - re_linksUnten.minX - 20,
+                                    height: re_rechtsOben.maxY - re_linksUnten.minY - 20))
+            let farbe = auto.farbe
+            farbe.setFill()
+            autoPath.fill()
+            NSColor.black.setStroke()
+            autoPath.lineWidth = 5
+            autoPath.stroke()
+        }
+        
+    }
+    
+    func erzeugeBasisQuadrat (füllFarbe: NSColor, randFarbe: NSColor, zeichnen: Bool, _ dirtyRect: NSRect) -> NSRect {
+        
+        let basisQuadratSeitenlänge = min(dirtyRect.height,dirtyRect.width) - 50
+        
+        let basisQuadrat = NSRect(
+            x: (dirtyRect.width - basisQuadratSeitenlänge) / 2,
+            y: (dirtyRect.height - basisQuadratSeitenlänge) / 2,
+            width: basisQuadratSeitenlänge,
+            height: basisQuadratSeitenlänge)
+        
+        if zeichnen {
+            let bQ = NSBezierPath()
+            bQ.appendRect(basisQuadrat)
+            füllFarbe.set()
+            bQ.fill()
+            
+            randFarbe.set()
+            bQ.lineWidth = 10
+            bQ.stroke()
+        }
+        
+        // Exit-Rechteck
+        
+        let exitRechteck = NSRect(
+            x: basisQuadrat.maxX - 6,
+            y: basisQuadrat.minY + basisQuadrat.height * 2 / 6 ,
+            width: 11, height: basisQuadrat.height / 6 + 2)
+        let eR = NSBezierPath()
+        eR.appendRect(exitRechteck)
+        NSColor.white.set()
+        eR.fill()
+        
+        
+        return basisQuadrat
+      
+    }
+    
+    func erzeugeKleinesQuadrat (waagerecht w: Int, senkrecht s: Int, farbe: NSColor, zeichnen: Bool, _ dirtyRect: NSRect) -> NSRect {
+        
+        let basisQuadratSeitenlängeInnen = min(dirtyRect.height,dirtyRect.width) - 65
+        
+        let innenQuadrat = NSRect(
+            x: (dirtyRect.width - basisQuadratSeitenlängeInnen) / 2,
+            y: (dirtyRect.height - basisQuadratSeitenlängeInnen) / 2,
+            width: basisQuadratSeitenlängeInnen,
+            height: basisQuadratSeitenlängeInnen)
+        
+        let kleinesQuadrat = NSRect(
+            x: innenQuadrat.minX + (CGFloat(w) - 1) * innenQuadrat.width / 6 + 2,
+            y: innenQuadrat.minY + (CGFloat(s) - 1) * innenQuadrat.height / 6 + 2,
+            width: innenQuadrat.width / 6 - 4,
+            height: innenQuadrat.height / 6 - 4)
+        
+        if zeichnen {
+            let kQ = NSBezierPath()
+            kQ.appendRect(kleinesQuadrat)
+            farbe.set()
+            kQ.fill()
+        }
+        return kleinesQuadrat
+    }
+}
